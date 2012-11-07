2feb34b24c8a5384a75db472403b0dd364bb0ce0
mst <mst@trustpilot.com>
2012-11-07 12:26:02 +0100

    User.IsVerified property is replaced by Review.IsVerified

--- samples/PluginsMVC3Sample/Models/Trustpilot/User.cs
+++ samples/PluginsMVC3Sample/Models/Trustpilot/User.cs
@@ -1,15 +1,23 @@
-﻿using System.Collections.Generic;
-
-namespace PluginsMVC3Sample.Models.Trustpilot
+﻿namespace PluginsMVC3Sample.Models.Trustpilot
 {
-	public class User
-	{
-		public string Name { get; set; }
+    using System;
+    using System.Collections.Generic;
+
+    public class User
+    {
+        public string Name { get; set; }
+
         public string City { get; set; }
+
         public string Locale { get; set; }
-		public int ReviewCount { get; set; }
-		public bool IsVerified { get; set; }
+
+        public int ReviewCount { get; set; }
+
+        [Obsolete("It is replaced by Review.IsVerified")]
+        public bool IsVerified { get; set; }
+
         public bool HasImage { get; set; }
+
         public Dictionary<string, string> ImageUrls { get; set; }
-	}
+    }
 }
